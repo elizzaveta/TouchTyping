9a21b5a743d19a50a0f15bb1b859efee34ee8041
Yelizaveta <yelizaveta.timchenko21@gmail.com>
2023-03-22 13:41:23 +0200

    [#12] - Added Cheat Sheet Page. Added link to this page to header

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -4,9 +4,13 @@ import styles from "./navbar.module.css"
 function Navbar() {
     return (
         <header className={styles.wrapper}>
-            <h2 className={styles.name}>Typer</h2>
+            <Link href="/">
+                <h2 className={styles.name}>Typer</h2>
+                <p className={styles.description}>Touch Typing Practice</p>
+            </Link>
             <div className={styles.nav}>
                 <Link href="/"><b>Home</b></Link>
+                <Link href="/cheat-sheet"><b>CheatSheet</b></Link>
                 <Link href="/about"><b>About</b></Link>
                 <Link href="/progress"><b>Progress</b></Link>
             </div>
